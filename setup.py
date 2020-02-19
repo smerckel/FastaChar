b7dbcedcb18f768ce8c2fa238fd144bb6cd44ac8
Lucas Merckelbach <lucas.merckelbach@hzg.de>
2020-02-19 15:16:07 +0100

    Updated READMe and documentation.

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
 from setuptools import setup
 
+long_description='''
+Fastachar
+=========
+
+**Fastachar** is a graphical user interface that allows a simple
+comparison of two sets of DNA sequences. A typical working example
+is to have a selection of DNA sequences of different, but related
+species. These sequences are divided in a set A, which are thought
+to be a single taxon (species) and a set B, which is a collection of
+different taxa. Using the program, it can be established easily whether:
+
+* the DNA sequences in set A are all identical, and if not, list the
+  differences for those positions in the DNA sequences.
+
+* set B has positions in the DNA that are different from those in
+  set A,  across **all** species in set B.
+
+**Fastachar** takes as input the alignments stored in a single file of
+the fasta format. **Fastachar** is not a tool to align sequences. It is
+assumed that the sequences were previously aligned with different algorithms 
+(e.g. MUSCLE) and the most reliable alignment was chosen.
+'''
+
 setup(name="fastachar",
       version="0.2.0",
       packages = ['fastachar'],
@@ -11,10 +34,8 @@ setup(name="fastachar",
       author="Lucas Merckelbach",
       author_email="lucas.merckelbach@hzg.de",
       description="A simple program with GUI to compare dna sequences",
-      long_description="""A simple python module with graphical user interface
-that allows to compare a group of aligned DNA sequences with another group of
-aligned DNA sequences. Different operations can be specified such as find
-differences or correspondences.""",
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
     url='http://cubic-l.science/fastachar.html',
     classifiers=[
         'Development Status :: 4 - Beta',
